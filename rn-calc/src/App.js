17587d5d4ce3bdd0727b274845928f62bf159f42
Beomjun Kim <alchemist85k@gmail.com>
2022-08-14 16:44:11 -0700

    2.2

--- rn-calc/src/App.js
+++ rn-calc/src/App.js
@@ -2,9 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const isError = false;
+
   return (
     <View style={styles.container}>
-      <Text>RN Calc App</Text>
+      <Text style={styles.text}>RN Calc App</Text>
+      <Text style={styles.text}>RN Calc App 2</Text>
+      <Text style={[styles.text, styles.error]}>Error Message</Text>
+      <Text style={[styles.error, styles.text]}>Error Message</Text>
+      <Text style={[styles.text, isError && styles.error]}>Error Message</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +22,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: '700',
+    color: 'green',
+  },
+  error: {
+    color: 'red',
   },
 });
