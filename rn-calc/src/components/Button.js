ba37010748eca0b4afe31f568d5a00238fcde47e
Beomjun Kim <alchemist85k@gmail.com>
2022-08-15 15:33:29 -0700

    2.6

--- rn-calc/src/components/Button.js
+++ rn-calc/src/components/Button.js
@@ -1,8 +1,22 @@
-import { Text } from 'react-native';
+import { Pressable, Text } from 'react-native';
 import PropTypes from 'prop-types';
 
 const Button = ({ title }) => {
-  return <Text>{title}</Text>;
+  return (
+    <Pressable
+      onPressIn={() => console.log('In')}
+      onPressOut={() => console.log('Out')}
+      onPress={() => console.log('onPress')}
+      onLongPress={() => console.log('Long')}
+      delayLongPress={2000}
+      style={({ pressed }) => [
+        { backgroundColor: 'red' },
+        pressed && { backgroundColor: 'orange', opacity: 0.3 },
+      ]}
+    >
+      <Text>{title}</Text>
+    </Pressable>
+  );
 };
 
 Button.defaultProps = {
