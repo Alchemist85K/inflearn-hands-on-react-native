0548726a56b456e9951534aab426fb57528e2732
Beomjun Kim <alchemist85k@gmail.com>
2022-08-20 15:36:03 -0700

    4.1

---
+++ rn-todo/src/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="dark" />
+      <Text style={{ fontSize: 60 }}>TODO App</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+export default App;
