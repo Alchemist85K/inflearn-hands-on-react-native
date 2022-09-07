f04f070f5aa8a375c9a494136cc327306dc289b3
Beomjun Kim <alchemist85k@gmail.com>
2022-09-07 12:30:30 -0700

    5.5

--- rn-todo/src/navigations/AuthStack.js
+++ rn-todo/src/navigations/AuthStack.js
@@ -1,5 +1,5 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { WHITE } from '../colors';
+import { PRIMARY, WHITE } from '../colors';
 import ListScreen from '../screens/ListScreen';
 import SignInScreen from '../screens/SignInScreen';
 
@@ -8,13 +8,17 @@ const Stack = createNativeStackNavigator();
 const AuthStack = () => {
   return (
     <Stack.Navigator
-      initialRouteName={'SignIn'}
+      initialRouteName={'Home'}
       screenOptions={{
         contentStyle: { backgroundColor: WHITE },
+        headerTitleAlign: 'center',
+        headerTintColor: PRIMARY.DEFAULT,
+        headerTitleStyle: { fontWeight: '700' },
+        title: 'TODO List',
       }}
     >
       <Stack.Screen name={'List'} component={ListScreen} />
-      <Stack.Screen name={'SignIn'} component={SignInScreen} />
+      <Stack.Screen name={'Home'} component={SignInScreen} />
     </Stack.Navigator>
   );
 };
