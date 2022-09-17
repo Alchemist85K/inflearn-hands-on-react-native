a06c4d657b8c7a9ae66d5960ab78b8346673b7b1
Beomjun Kim <alchemist85k@gmail.com>
2022-09-17 15:28:34 -0700

    6.1

--- rn-todo/src/screens/ListScreen.js
+++ rn-todo/src/screens/ListScreen.js
@@ -1,18 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 const ListScreen = () => {
+  const todos = [];
+  for (let i = 1; i < 501; i++) {
+    todos.push({ id: i, task: `task ${i}`, number: i });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={{ fontSize: 30 }}>ListScreen</Text>
-    </View>
+    <FlatList
+      data={todos}
+      renderItem={({ item }) => {
+        console.log(item.id);
+        return (
+          <View style={{ paddingHorizontal: 20, paddingVertical: 10 }}>
+            <Text style={{ fontSize: 20 }}>{item.task}</Text>
+          </View>
+        );
+      }}
+      keyExtractor={(item) => item.number.toString()}
+    />
+    // <ScrollView style={styles.container}>
+    //   {todos.map((item) => {
+    //     console.log(item.id);
+    //     return (
+    //       <View
+    //         key={item.id}
+    //         style={{ paddingHorizontal: 20, paddingVertical: 10 }}
+    //       >
+    //         <Text style={{ fontSize: 20 }}>{item.task}</Text>
+    //       </View>
+    //     );
+    //   })}
+    // </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 });
 
