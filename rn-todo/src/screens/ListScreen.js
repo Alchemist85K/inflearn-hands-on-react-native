1aeb7333b69416a1c5f850cc9aff5349a514c697
Beomjun Kim <alchemist85k@gmail.com>
2022-09-20 15:37:31 -0700

    6.13

--- rn-todo/src/screens/ListScreen.js
+++ rn-todo/src/screens/ListScreen.js
@@ -1,34 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import EmptyList from '../components/EmptyList';
 import InputFAB from '../components/InputFAB';
 import List from '../components/List';
 import 'react-native-get-random-values';
 import { nanoid } from 'nanoid';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { View } from 'react-native';
+import { Alert, View } from 'react-native';
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
 
 const ListScreen = () => {
-  const [todos, setTodos] = useState([
-    { id: '1', task: 'task 1', isDone: false },
-    { id: '2', task: 'task 2', isDone: false },
-    { id: '3', task: 'task 3', isDone: false },
-    { id: '4', task: 'task 4', isDone: false },
-    { id: '5', task: 'task 5', isDone: false },
-    { id: '6', task: 'task 6', isDone: false },
-    { id: '7', task: 'task 7', isDone: false },
-    { id: '8', task: 'task 8', isDone: false },
-    { id: '9', task: 'task 9', isDone: false },
-    { id: '10', task: 'task 10', isDone: false },
-    { id: '11', task: 'task 11', isDone: false },
-    { id: '12', task: 'task 12', isDone: false },
-  ]);
+  const [todos, setTodos] = useState([]);
   const [isBottom, setIsBottom] = useState(false);
   const { bottom } = useSafeAreaInsets();
+  const { getItem, setItem } = useAsyncStorage('todos');
+
+  const save = async (data) => {
+    try {
+      await setItem(JSON.stringify(data));
+      setTodos(data);
+    } catch (e) {
+      Alert.alert('저장 실패');
+    }
+  };
+
+  const load = async () => {
+    try {
+      const data = await getItem();
+      const todos = JSON.parse(data || '[]');
+      setTodos(todos);
+    } catch (e) {
+      Alert.alert('불러오기 실패');
+    }
+  };
+
+  useEffect(() => {
+    load();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const onInsert = (task) => {
     const id = nanoid();
     const newTask = { id, task, isDone: false };
-    setTodos((prev) => [newTask, ...prev]);
+    save([newTask, ...todos]);
   };
 
   return (
