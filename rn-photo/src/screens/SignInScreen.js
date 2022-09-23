bb330d69ce1eea2fc6902d77240abedf9cc215e1
Beomjun Kim <alchemist85k@gmail.com>
2022-09-23 14:26:09 -0700

    7.7

--- rn-photo/src/screens/SignInScreen.js
+++ rn-photo/src/screens/SignInScreen.js
@@ -4,44 +4,47 @@ import { AuthRoutes } from '../navigations/routes';
 import Input, { InputTypes } from '../components/Input';
 import { useState } from 'react';
 import Button from '../components/Button';
+import SafeInputView from '../components/SafeInputView';
 
 const SignInScreen = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>SignInScreen</Text>
+    <SafeInputView>
+      <View style={styles.container}>
+        <Text style={styles.title}>SignInScreen</Text>
 
-      <Input
-        inputType={InputTypes.EMAIL}
-        value={email}
-        onChangeText={(text) => setEmail(text.trim())}
-        styles={{
-          container: { paddingHorizontal: 20, marginBottom: 20 },
-        }}
-      />
+        <Input
+          inputType={InputTypes.EMAIL}
+          value={email}
+          onChangeText={(text) => setEmail(text.trim())}
+          styles={{
+            container: { paddingHorizontal: 20, marginBottom: 20 },
+          }}
+        />
 
-      <Input
-        inputType={InputTypes.PASSWORD}
-        value={password}
-        onChangeText={(text) => setPassword(text.trim())}
-        styles={inputStyles}
-      />
+        <Input
+          inputType={InputTypes.PASSWORD}
+          value={password}
+          onChangeText={(text) => setPassword(text.trim())}
+          styles={inputStyles}
+        />
 
-      <Button
-        title={'SIGNIN'}
-        onPress={() => {
-          navigation.navigate(AuthRoutes.SIGN_UP);
-        }}
-        styles={{
-          container: {
-            paddingHorizontal: 20,
-            marginTop: 20,
-          },
-        }}
-      />
-    </View>
+        <Button
+          title={'SIGNIN'}
+          onPress={() => {
+            navigation.navigate(AuthRoutes.SIGN_UP);
+          }}
+          styles={{
+            container: {
+              paddingHorizontal: 20,
+              marginTop: 20,
+            },
+          }}
+        />
+      </View>
+    </SafeInputView>
   );
 };
 
