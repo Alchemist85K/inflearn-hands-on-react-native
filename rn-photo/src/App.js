6ffe4a398833b29765d5efec98299482e3348cc8
Beomjun Kim <alchemist85k@gmail.com>
2022-09-28 16:04:35 -0700

    7.20

--- rn-photo/src/App.js
+++ rn-photo/src/App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import Navigation from './navigations';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
+import { View } from 'react-native';
+import { Asset } from 'expo-asset';
 
 const App = () => {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Asset.fromModule(require('../assets/cover.png')).downloadAsync();
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e);
+      } finally {
+        setIsReady(true);
+      }
+    })();
+  }, []);
+
+  const onReady = async () => {
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
+  };
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
-    <>
+    <View style={{ flex: 1 }} onLayout={onReady}>
       <StatusBar style={'dark'} />
       <Navigation />
-    </>
+    </View>
   );
 };
 
