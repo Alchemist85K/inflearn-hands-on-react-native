dae30b7caf9107ddc8f9414c7b89f3f7b5f80227
Beomjun Kim <alchemist85k@gmail.com>
2022-09-30 16:38:57 -0700

    8.8

--- rn-photo/src/App.js
+++ rn-photo/src/App.js
@@ -1,49 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import Navigation from './navigations';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
-import { LogBox, View } from 'react-native';
-import { Asset } from 'expo-asset';
-import { initFirebase } from './api/firebase';
+import { LogBox } from 'react-native';
 import { UserProvier } from './contexts/UserContext';
 
 const App = () => {
   LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core']);
 
-  const [isReady, setIsReady] = useState(false);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        await SplashScreen.preventAutoHideAsync();
-        await Asset.fromModule(require('../assets/cover.png')).downloadAsync();
-
-        initFirebase();
-      } catch (e) {
-        // eslint-disable-next-line no-console
-        console.log(e);
-      } finally {
-        setIsReady(true);
-      }
-    })();
-  }, []);
-
-  const onReady = async () => {
-    if (isReady) {
-      await SplashScreen.hideAsync();
-    }
-  };
-
-  if (!isReady) {
-    return null;
-  }
-
   return (
     <UserProvier>
-      <View style={{ flex: 1 }} onLayout={onReady}>
-        <StatusBar style={'dark'} />
-        <Navigation />
-      </View>
+      <StatusBar style={'dark'} />
+      <Navigation />
     </UserProvier>
   );
 };
--- rn-photo/src/navigations/index.js
+++ rn-photo/src/navigations/index.js
@@ -1,13 +1,54 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
+import { onAuthStateChanged } from '../api/auth';
 import { useUserState } from '../contexts/UserContext';
 import AuthStack from './AuthStack';
 import MainStack from './MainStack';
+import { Asset } from 'expo-asset';
+import { initFirebase } from '../api/firebase';
+import * as SplashScreen from 'expo-splash-screen';
 
 const Navigation = () => {
-  const [user] = useUserState();
+  const [user, setUser] = useUserState();
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Asset.fromModule(
+          require('../../assets/cover.png')
+        ).downloadAsync();
+
+        initFirebase();
+
+        const unsubscribe = onAuthStateChanged((user) => {
+          if (user) {
+            setUser(user);
+          }
+          setIsReady(true);
+          unsubscribe();
+        });
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e);
+        setIsReady(true);
+      }
+    })();
+  }, [setUser]);
+
+  const onReady = async () => {
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
+  };
+
+  if (!isReady) {
+    return null;
+  }
 
   return (
-    <NavigationContainer>
+    <NavigationContainer onReady={onReady}>
       {user.uid ? <MainStack /> : <AuthStack />}
     </NavigationContainer>
   );
