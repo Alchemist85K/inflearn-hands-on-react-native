37ffe199dc2cff394c46c21ad032a2f838439d2d
Beomjun Kim <alchemist85k@gmail.com>
2022-10-01 16:00:02 -0700

    8.10

--- rn-photo/src/navigations/ContentTab.js
+++ rn-photo/src/navigations/ContentTab.js
@@ -4,16 +4,55 @@ import ListScreen from '../screens/ListScreen';
 import MapScreen from '../screens/MapScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import { ContentRoutes } from './routes';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { GRAY, PRIMARY } from '../colors';
 
 const Tab = createBottomTabNavigator();
 
+const getTabBarIcon = ({ focused, color, size, name }) => {
+  const iconName = focused ? name : `${name}-outline`;
+  return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+};
+
 const ContentTab = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name={ContentRoutes.HOME} component={HomeScreen} />
-      <Tab.Screen name={ContentRoutes.LIST} component={ListScreen} />
-      <Tab.Screen name={ContentRoutes.MAP} component={MapScreen} />
-      <Tab.Screen name={ContentRoutes.PROFILE} component={ProfileScreen} />
+    <Tab.Navigator
+      initialRouteName={ContentRoutes.HOME}
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: PRIMARY.DARK,
+        tabBarInactiveTintColor: GRAY.DARK,
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tab.Screen
+        name={ContentRoutes.HOME}
+        component={HomeScreen}
+        options={{
+          tabBarIcon: (props) => getTabBarIcon({ ...props, name: 'home' }),
+        }}
+      />
+      <Tab.Screen
+        name={ContentRoutes.LIST}
+        component={ListScreen}
+        options={{
+          tabBarIcon: (props) => getTabBarIcon({ ...props, name: 'post' }),
+        }}
+      />
+      <Tab.Screen
+        name={ContentRoutes.MAP}
+        component={MapScreen}
+        options={{
+          tabBarIcon: (props) => getTabBarIcon({ ...props, name: 'map' }),
+        }}
+      />
+      <Tab.Screen
+        name={ContentRoutes.PROFILE}
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: (props) => getTabBarIcon({ ...props, name: 'account' }),
+        }}
+      />
     </Tab.Navigator>
   );
 };
