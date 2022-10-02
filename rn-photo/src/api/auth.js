fc1a6ea1d4eb35a81e7cbb04599cb339d2f0ad6c
Beomjun Kim <alchemist85k@gmail.com>
2022-10-02 14:53:54 -0700

    8.15

--- rn-photo/src/api/auth.js
+++ rn-photo/src/api/auth.js
@@ -8,6 +8,9 @@ import {
   updateProfile,
 } from 'firebase/auth';
 
+const PHOTO_URL =
+  'https://firebasestorage.googleapis.com/v0/b/rn-photo-b092b.appspot.com/o/profile.png?alt=media';
+
 export const getAuthErrorMessages = (errorCode) => {
   switch (errorCode) {
     case AuthErrorCodes.USER_DELETED:
