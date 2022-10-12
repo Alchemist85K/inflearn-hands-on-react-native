083eee0bfdbd03a41f037220b007c24e67c29029
Beomjun Kim <alchemist85k@gmail.com>
2022-10-12 14:36:30 -0700

    10.18

--- rn-photo/src/App.js
+++ rn-photo/src/App.js
@@ -2,15 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import Navigation from './navigations';
 import { LogBox } from 'react-native';
 import { UserProvier } from './contexts/UserContext';
+import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 
 const App = () => {
   LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core']);
 
   return (
-    <UserProvier>
-      <StatusBar style={'dark'} />
-      <Navigation />
-    </UserProvier>
+    <ActionSheetProvider>
+      <UserProvier>
+        <StatusBar style={'dark'} />
+        <Navigation />
+      </UserProvier>
+    </ActionSheetProvider>
   );
 };
 
--- rn-photo/src/components/PostItem.js
+++ rn-photo/src/components/PostItem.js
@@ -1,24 +1,65 @@
-import { StyleSheet, Text, useWindowDimensions, View } from 'react-native';
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  useWindowDimensions,
+  View,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import { memo } from 'react';
 import ImageSwiper from './ImageSwiper';
 import FastImage from './FastImage';
 import { GRAY, PRIMARY, WHITE } from '../colors';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useUserState } from '../contexts/UserContext';
+import { useActionSheet } from '@expo/react-native-action-sheet';
+
+const ActionSheetOptions = {
+  options: ['삭제', '수정', '취소'],
+  cancelButtonIndex: 2,
+  destructiveButtonIndex: 0,
+};
 
 const PostItem = memo(({ post }) => {
   const width = useWindowDimensions().width;
+  const [user] = useUserState();
+  const { showActionSheetWithOptions } = useActionSheet();
+
+  const onPressActionSheet = (idx) => {
+    if (idx === 0) {
+      //
+    } else if (idx === 1) {
+      //
+    }
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <FastImage
-          source={{ uri: post.user.photoURL }}
-          style={styles.profilePhoto}
-        />
-        <Text style={styles.nickname}>
-          {post.user.displayName ?? 'nickname'}
-        </Text>
+        <View style={styles.profile}>
+          <FastImage
+            source={{ uri: post.user.photoURL }}
+            style={styles.profilePhoto}
+          />
+          <Text style={styles.nickname}>
+            {post.user.displayName ?? 'nickname'}
+          </Text>
+        </View>
+
+        {post.user.uid === user.uid && (
+          <Pressable
+            hitSlop={10}
+            onPress={() =>
+              showActionSheetWithOptions(ActionSheetOptions, onPressActionSheet)
+            }
+          >
+            <MaterialCommunityIcons
+              name="dots-horizontal"
+              size={24}
+              color={GRAY.DARK}
+            />
+          </Pressable>
+        )}
       </View>
 
       <View style={{ width, height: width }}>
@@ -52,8 +93,13 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
     paddingHorizontal: 10,
     paddingBottom: 10,
+  },
+  profile: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   profilePhoto: {
     width: 40,
